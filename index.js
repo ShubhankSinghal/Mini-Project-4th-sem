dfbf2f3904905fc395bed330b2c95c354c164572
ShubhankSinghal <shubhank.singhal98@gmail.com>
2020-05-13 19:21:32 +0530

    Cloudant database

--- index.js
+++ index.js
@@ -2,25 +2,50 @@ var nodemailer = require('nodemailer');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const Cloudant = require('@cloudant/cloudant');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+var cloudant = new Cloudant({ url: 'cloudantURL', plugins: { iamauth: { iamApiKey: 'APIkey' } } });
+
+cloudant.db.list(function(err, body) {
+  body.forEach(function(db) {
+   console.log(db);
+  });
+});
+
+var counter = 0;
 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'yourmail@gmail.com',
+    user: 'youremail@gmail.com',
     pass: 'password'
   }
 });
 
 app.post('/mail', (req, res) => {
-    console.log(`First Name: ${req.body.fname}\nLast Name: ${req.body.lname}\nEmail: ${req.body.email}\nMessage:\n${req.body.message}`);
+
+    var fname = `${req.body.fname}`;
+    var lname = `${req.body.lname}`;
+    var email = `${req.body.email}`;
+    var message = `${req.body.message}`;
+
+    console.log(`First Name: ${fname}\nLast Name: ${lname}\nEmail: ${email}\nMessage:\n${message}`);
+
+    cloudant.use('mini_project').insert({_id: counter.toString(), "Name": fname+lname , "Email" : email, "Message": message }, function(err, data) {
+      console.log('Error:', err);
+      console.log('Data:', data);
+      if(!err){
+        counter++;
+      }
+    });
 
     var mailOptions = {
-      from: 'yourmail@gmail.com',
-      to: 'yourmail@gmail.com',
-      subject: 'Query on B.Tech. GEU First Year Website',
-      text: `First Name: ${req.body.fname}\nLast Name: ${req.body.lname}\nEmail: ${req.body.email}\nMessage: ${req.body.message}`
+      from: 'youremail@gmail.com',
+      to: 'youremail@gmail.com',
+      subject: 'B.Tech. GEU First Year Website',
+      text: `First Name: ${fname}\nLast Name: ${lname}\nEmail: ${email}\nMessage: ${message}`
     };
     
     transporter.sendMail(mailOptions, function(error, info){
